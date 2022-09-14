1dbc416813a03b6d440b51e0e40b8689d6eca3d7
10Derozan <fupeng.antik@gmail.com>
2022-09-14 17:22:50 +0800

    ci: change release and runs-on

--- scripts/release.sh
+++
@@ -1,8 +0,0 @@
-npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
-
-rush change
-rush version --bump
-NPM_TOKEN=xxxx rush publish --add-commit-details  --publish --include-all
-
-git add .
-git commit -m 'chore: release'
