d9cd8a2787253715e7a089ea1a97bc64cdf723a6
10Derozan <fupeng.antik@gmail.com>
2022-09-14 20:49:11 +0800

    test: remove mocha list func

--- tests/fixture/.mocharc.js
+++ tests/fixture/.mocharc.js
@@ -5,6 +5,5 @@ module.exports = {
   require: require.resolve('tsm'),
   extensions: ['.ts', '.tsx', '.js', '.jsx'],
   spec: [path.join(__dirname, './src/**/*.{spec,test}.*')],
-  file: [path.join(__dirname, './setupMocha.js')],
   timeout: 100000,
 };
