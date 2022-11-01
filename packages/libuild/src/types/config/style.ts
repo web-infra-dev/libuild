03825285f176194136ddd70fbb3f35938c85974c
10Derozan <fupeng.antik@gmail.com>
2022-11-01 17:36:03 +0800

    feat: support postcss-modules option

--- packages/libuild/src/types/config/style.ts
+++ packages/libuild/src/types/config/style.ts
@@ -1,11 +1,34 @@
 import type CleanCss from 'clean-css';
-import type { AcceptedPlugin, ProcessOptions } from 'postcss';
+import type { AcceptedPlugin, ProcessOptions, Plugin } from 'postcss';
 import { Options as sassOptions } from 'sass';
+
+type LocalsConventionFunction = (originalClassName: string, generatedClassName: string, inputFile: string) => string;
+
+type GenerateScopedNameFunction = (name: string, filename: string, css: string) => string;
+
+declare class Loader {
+  constructor(root: string, plugins: Plugin[]);
+
+  fetch(file: string, relativeTo: string, depTrace: string): Promise<{ [key: string]: string }>;
+
+  finalSource?: string | undefined;
+}
 
 export type PostcssOptions = {
   processOptions?: ProcessOptions;
   plugins?: AcceptedPlugin[];
   autoModules?: boolean | RegExp;
+  modules?: {
+    localsConvention?: 'camelCase' | 'camelCaseOnly' | 'dashes' | 'dashesOnly' | LocalsConventionFunction;
+    scopeBehaviour?: 'global' | 'local';
+    globalModulePaths?: RegExp[];
+    generateScopedName?: string | GenerateScopedNameFunction;
+    hashPrefix?: string;
+    exportGlobals?: boolean;
+    root?: string;
+    resolve?: (file: string) => string | Promise<string>;
+    Loader?: typeof Loader;
+  };
 };
 
 export type { sassOptions };
