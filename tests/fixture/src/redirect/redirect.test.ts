19b63ebac3d1e7ce2e7cd30330b21813d2e3a91a
10Derozan <fupeng.antik@gmail.com>
2022-12-23 15:58:50 +0800

    fix: replace css module filename (#17)

--- tests/fixture/src/redirect/redirect.test.ts
+++ tests/fixture/src/redirect/redirect.test.ts
@@ -16,7 +16,7 @@ describe('fixture:redirect', () => {
     const jsOutput = bundler.getJSOutput();
     Object.entries(jsOutput).map(([path, chunk]) => {
       if (path.endsWith('.module.js')) {
-        expect(chunk.contents.includes('import "./style.module.css"')).to.be.true;
+        expect(chunk.contents.includes('import "./style_module.css"')).to.be.true;
       } else {
         expect(chunk.contents.includes('import a from "./style.module"')).to.be.true;
         expect(chunk.contents.includes('import b from "./assets/logo.5d5d9eef.svg"')).to.be.true;
