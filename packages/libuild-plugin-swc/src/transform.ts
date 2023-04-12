6fb6bd9e61d54fa03b2caf9e4790f1d78fa90a29
targeral <targeral@outlook.com>
2023-04-12 19:04:09 +0800

    feat: add useJscParser config

--- packages/libuild-plugin-swc/src/transform.ts
+++ packages/libuild-plugin-swc/src/transform.ts
@@ -2,14 +2,59 @@ import type { LibuildPlugin } from '@modern-js/libuild';
 import { Compiler, TransformConfig } from '@modern-js/swc-plugins';
 import { isJsExt, resolvePathAndQuery, isJsLoader, deepMerge } from '@modern-js/libuild-utils';
 
-export const transformPlugin = (options?: TransformConfig): LibuildPlugin => {
+export type Config = {
+  // In some cases it is necessary to use the specified parser. eg:
+  // code: `import { Type } from 'xx'`, `Type` is a type definition.
+  // Using swc's ts parser, these type definition can be removed.
+  useJscParser?: 'auto' | 'typescript' | 'ecmascript';
+};
+
+export const getJscParser = (
+  useJscParser: Config['useJscParser'],
+  options: {
+    isTs: boolean;
+    enableTsx: boolean;
+  }
+) => {
+  const { enableTsx, isTs } = options;
+  if (useJscParser === 'typescript') {
+    return {
+      syntax: 'typescript',
+      tsx: enableTsx,
+      decorators: true,
+    };
+  }
+  if (useJscParser === 'ecmascript') {
+    return {
+      syntax: 'ecmascript',
+      jsx: true,
+      decorators: true,
+    };
+  }
+  return isTs
+    ? {
+        syntax: 'typescript',
+        tsx: enableTsx,
+        decorators: true,
+      }
+    : {
+        syntax: 'ecmascript',
+        jsx: true,
+        decorators: true,
+      };
+};
+
+export const transformPlugin = (options?: TransformConfig, config?: Config): LibuildPlugin => {
   const pluginName = 'libuild:swc-transform';
+  const { useJscParser = 'auto' } = config ?? {};
   return {
     name: pluginName,
     apply(compiler) {
       compiler.hooks.transform.tapPromise({ name: pluginName }, async (args) => {
         const { originalFilePath } = resolvePathAndQuery(args.path);
         const isTs = args.loader === 'tsx' || args.loader === 'ts' || /\.tsx?$/i.test(originalFilePath);
+        const enableTsx = args.loader === 'tsx' || /\.tsx$/i.test(originalFilePath);
+
         if (isJsExt(originalFilePath) || isJsLoader(args.loader)) {
           const mergeOptions: TransformConfig = deepMerge(
             {
@@ -19,17 +64,7 @@ export const transformPlugin = (options?: TransformConfig): LibuildPlugin => {
               swcrc: false,
               configFile: false,
               jsc: {
-                parser: isTs
-                  ? {
-                      syntax: 'typescript',
-                      tsx: true,
-                      decorators: true,
-                    }
-                  : {
-                      syntax: 'ecmascript',
-                      jsx: true,
-                      decorators: true,
-                    },
+                parser: getJscParser(useJscParser, { isTs, enableTsx }),
                 target: 'es2022',
               },
               isModule: 'unknown',
