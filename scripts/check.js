d44970c391ac9d8945c2a09d47d509112339aff1
chenjiahan <chenjiahan.jait@bytedance.com>
2023-06-01 14:31:34 +0800

    fix(CI): check changeset failed

--- scripts/check.js
+++ scripts/check.js
@@ -1,14 +1,28 @@
 const { promisify } = require('util');
-const { exec: execOrig } = require('child_process');
+const { exec: execOrig, execSync } = require('child_process');
 
 const exec = promisify(execOrig);
 
+function getCurrentBranch() {
+  return execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+}
+
 async function main() {
   console.log('Checking for changeset...');
-  console.log(`GITHUB_HEAD_REF is ${process.argv}`);
-  if (process.argv[2].startsWith('bump') || process.argv[2].startsWith('release')) {
+
+  const GITHUB_HEAD_REF = process.argv[2];
+
+  if (GITHUB_HEAD_REF) {
+    console.log(`GITHUB_HEAD_REF is ${GITHUB_HEAD_REF}`);
+  }
+
+  const branchName = GITHUB_HEAD_REF || getCurrentBranch();
+
+  if (branchName.startsWith('bump') || branchName.startsWith('release')) {
+    console.log('Skipped in current branch: ', branchName);
     return;
   }
+
   const { stdout } = await exec('rush change -v');
   console.log(stdout);
 }
