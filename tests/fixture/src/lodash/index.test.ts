d8a20b9c8e30456e916f9525be2f82c9f7c69491
targeral <targeral@outlook.com>
2023-07-12 19:17:00 +0800

    chore: update @modern-js/swc-plugins 0.5.0 (#61)

--- tests/fixture/src/lodash/index.test.ts
+++ tests/fixture/src/lodash/index.test.ts
@@ -12,8 +12,8 @@ describe('fixture:lodash', () => {
     await bundler.build();
     const jsOutput = bundler.getJSOutput();
     Object.entries(jsOutput).map(([path, chunk]) => {
-      expect(chunk.contents.includes('import _map3 from "lodash-es/map"')).to.be.true;
-      expect(chunk.contents.includes('import _map2 from "lodash/map"')).to.be.true;
+      expect(chunk.contents.includes('import _lodash_map3 from "lodash-es/map"')).to.be.true;
+      expect(chunk.contents.includes('import _lodash_map2 from "lodash/map"')).to.be.true;
       expect(chunk.contents.includes('import addEs from "lodash-es/add"')).to.be.true;
       expect(chunk.contents.includes('import add from "lodash/add"')).to.be.true;
     });
