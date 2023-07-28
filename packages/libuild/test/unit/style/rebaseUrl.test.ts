825aed6d6c8e2e897d0ec9ab53b79a8d692809e7
10Derozan <fupeng.antik@gmail.com>
2023-07-28 17:24:50 +0800

    test(libuild): add variable test for rebaseUrl func (#67)

--- packages/libuild/test/unit/style/rebaseUrl.test.ts
+++ packages/libuild/test/unit/style/rebaseUrl.test.ts
@@ -28,4 +28,14 @@ describe('rebase', () => {
     });
     assert(result.contents?.includes?.('/a.png'), 'rewrite url with absolute url');
   });
+  it('do not replace variable', async () => {
+    const rootFile = path.resolve(__dirname, 'index.less');
+    const file = path.resolve(__dirname, 'less/variable.less');
+    let replace = false;
+    await rebaseUrls(file, path.dirname(rootFile), (id: string, dir: string) => {
+      replace = true;
+      return path.resolve(dir, id);
+    });
+    assert(!replace);
+  });
 });
